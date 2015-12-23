b807abe5a39e6eb843c4c4cb4a6d99950c6e8a2f
Bennie Mosher <benniemosher@gmail.com>
2015-12-23 09:25:06 -0700

    Adding more colors to the theme-settings.yml

--- theme-converter.rb
+++ theme-converter.rb
@@ -2,10 +2,11 @@ require 'yaml'
 settings = YAML::load_file(File.join(__dir__, 'theme-settings.yml'))
 
 sample_file = 'textmate/textmate.tmTheme.sample'
+new_file = "../#{settings['theme-folder-name']}/textmate/#{settings['theme-filename']}.tmTheme"
 sample_file_read = File.read(sample_file)
 
 settings.each do |key, value|
-	File.open('textmate/TheOne.tmTheme', 'w') do |file|
+	File.open(new_file, 'w') do |file|
 		file.puts sample_file_read.gsub!("{{ #{key} }}", value)
 	end
 end
